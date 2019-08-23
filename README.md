7f9a9119c551611d35e3ed80b32f044afe7d1ea2
pogolo <pogolo@Acer.localdomain>
2019-08-23 20:02:46 +0700

    Update WIP

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 #INProject
+
+Starting point
+
+Features:
+1. Store value
+2. Randomised value
+3. Interactive segments
+4. Reactive visual
+
+Segments:
+1. Gate: input; css (text box)
+2. Choose attributes: form; var (form parchment)
+3. Dice game: random value; compare value; count instances (machine game master)
+4. Choice: Passage scrolling activated button (dull long passage with blip to cut and choose option based on words)
+5. Letter hints: eventListener; button sequence (mystery letter with instruction)
+
+Gate:
+1. Fade-in background => Dynamic effect on click
+
+Choose attributes:
+1. Flaovur description
+2. Form => Assign Luck value => Conclude Luck value
+
+Dice:
+1. Flavour description
+2. Rolling dice => Alert pop-up => Luck calculation => Dice value
+3. Dice value reveal => Comparison => W/L
+
+Passage:
+1. Distraccting flavour passage
+2. Hidden clickable input => Morph passage => Morph screen
+3. Puzzle choice
--- function.js
+++ function.js
@@ -1,7 +1,79 @@
-let random = Math.floor(Math.random() * 6) + 1;
+let playerPool = [];
+let hostPool = [];
+var luckValue = 0;
+var dexterityValue = 0;
+
+// ==============attributes.html======================
+
+let calculate = () => {
+    luck = document.querySelectorAll('input[name="luck"]');
+    for(var i=0;i<luck.length;i++) {
+        if(luck.checked==true) {
+            luckValue += Number(luck[i].value);
+        }   
+    }
+    dexterity = document.querySelectorAll('input[name="dexterity"]');
+    for(var i=0;i<dexterity.length;i++) {
+        if(dexterity.checked==true) {
+            dexterityValue += Number(dexterity[i].value);
+        }   
+    }
+}
+
+// ==================dice.html========================
 
 let diceRoll = () => {
-    return random
-};
+    if(playerPool.length == 3) {
+        alert(`You'vve Enuv. Get outta 'ere`)
+        return window.location.href = 'passage.html';
+    }
 
-console.log(diceRoll)
+    alert(`Fate's hands are in motion...`);
+    
+    let playerRandom = Math.floor(Math.random() * 12) + 1;
+    // playerRandom += luckValue;
+    playerPool.push(playerRandom);
+    
+    let hostRandom = Math.floor(Math.random() * 6) + 6;
+    hostPool.push(hostRandom);
+
+    alert(playerRandom)
+}
+
+let reveal = () => {
+    alert('A bit of a sleight of hand')
+    
+    // playerRandom += dexterityValue;
+    let playerDiv = document.createElement('div');  
+    playerDiv.innerText = playerPool[playerPool.length - 1];
+    var playerInscribe = document.getElementById("playerRoll");
+    playerInscribe.appendChild(playerDiv);
+}
+
+let compare = () => {
+    let resultDiv = document.createElement('div');
+
+    if(playerPool[playerPool.length - 1] > hostPool[hostPool.length - 1]) {
+        alert(`The gods are with you`);
+        resultDiv.innerText = 'Won';
+        var resultInscribe = document.getElementById("resultRoll");
+        resultInscribe.appendChild(resultDiv);
+    }
+    else if(playerPool[playerPool.length - 1] < hostPool[hostPool.length - 1]) {
+        alert(`Despair...`);
+        resultDiv.innerText = 'Lose';
+        var resultInscribe = document.getElementById("resultRoll");
+        resultInscribe.appendChild(resultDiv);
+    }
+    else {
+        alert(`It's a match`);
+        resultDiv.innerText = 'Tie';
+        var resultInscribe = document.getElementById("resultRoll");
+        resultInscribe.appendChild(resultDiv);
+    }
+    let hostDiv = document.createElement('div');
+    hostDiv.innerText = hostPool[hostPool.length - 1];
+    var hostInscribe = document.getElementById("hostRoll");
+    hostInscribe.appendChild(hostDiv);
+}
+
